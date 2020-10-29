9f5e8bc354d630c0d06286706326029af87e47fa
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-10-29 19:33:54 -0400

    Change to class component and add router

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,21 @@
 import './App.scss';
-import { Homepage } from '../Homepage/Homepage'
+import {Homepage} from '../Homepage/Homepage'
+import React, {Component} from 'react'
+import {Router} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <Homepage />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return (
+      <div className="App">
+        <Homepage />
+      </div>
+    );
+  }
 }
 
 export default App;
