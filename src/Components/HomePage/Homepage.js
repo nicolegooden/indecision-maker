3b30196d705d82cefc8176d7eed766c5efefd05c
Orlando Murcio <ato.murcio@gmail.com>
2020-10-29 19:45:21 -0600

    Add activity containers

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -5,12 +5,16 @@ export function Homepage() {
     return (
         <div className='homepage-container'>
           <div className="homepage-inner-container">
-            <article></article>
-            <section>
-                {/* <article>Adult Activities</article>
-                <article>Family Activities</article> */}
-            </section>
+            <h1 className="app-title1">Indecision</h1>
+            <h1 className="app-title2">maker</h1>
           </div>
+            <section className='activities-info'>
+              <div className="activity-container">
+                <div className="inner-activity-container">
+
+                </div>
+              </div>
+            </section>
         </div>
     )
 }
