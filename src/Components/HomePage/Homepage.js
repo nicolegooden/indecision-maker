903f5a4c66eb24a098958dc2d3104b0d9c71093d
Orlando Murcio <ato.murcio@gmail.com>
2020-10-29 19:34:46 -0600

    Add div  elements for styling

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import './Homepage'
+import './Homepage.scss'
 
 export function Homepage() {
     return (
-        <main>
-            <article>HOLA</article>
+        <div className='homepage-container'>
+          <div className="homepage-inner-container">
+            <article></article>
             <section>
-                <article>Adult Activities</article>
-                <article>Family Activities</article>
+                {/* <article>Adult Activities</article>
+                <article>Family Activities</article> */}
             </section>
-        </main>
+          </div>
+        </div>
     )
 }
