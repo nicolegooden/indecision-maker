73cd67678ce02a08598fbd9fdb82285ead0cad15
Orlando Murcio <ato.murcio@gmail.com>
2020-10-29 22:27:34 -0600

    Add link to repos in nav-bar

--- src/Components/HomePage/Homepage.js
+++ src/Components/HomePage/Homepage.js
@@ -2,16 +2,23 @@ import React from 'react'
 import './Homepage.scss'
 import { ActivityCards }  from '../ActivityCards/ActivityCards'
 import { CgUserlane } from "react-icons/cg";
+import { FaServer, FaGithub} from "react-icons/fa";
 
 export function Homepage() {
     return (
         <div className='homepage-container'>
+
           <nav className="nav-bar">
-            <i className="app-logo"><CgUserlane className="logo"/></i>
+            <div className="app-logo">
+              <CgUserlane className="logo"/>
+            </div>
             <ul className="nav-links">
               <li className="link">About</li>
               <li className="link">Developers</li>
               <li className="link"></li>
+              <li className="link"><a className="link" href="https://github.com/nicolegooden/indecision-maker"><FaServer/></a></li>
+              <li className="link"><a href="https://github.com/Atos20/indecision-maker-server"><FaGithub/></a></li>
+              
             </ul>
           </nav>
 
