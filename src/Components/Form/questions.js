259dcec97987a5483b51d5db3bcdcda1c4c2e8b2
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-31 11:06:06 -0600

    Remove answers array from dataset

--- src/Components/Form/questions.js
+++ src/Components/Form/questions.js
@@ -2,19 +2,16 @@ export const questionSet = [
     {
         question: 'Does your group include kids?', 
         choices: ['yes', 'no'],
-        answers: [],
         activities: ['movies', 'board games', 'music', 'card games', 'podcasts']
     }, 
     {
         question: 'Which activities excite you right now?',
         choices: ['movies', 'board games', 'music', 'card games', 'podcasts'],
-        answers: [],
         activities: ['movies', 'board games', 'music', 'card games', 'podcasts'],
     },
     {
       question: 'How old is too old?',
       choices: [2010, 2000, 1990, 1980, "No such thing as too old"],
-      answers: [],
       activities: ['music', 'podcasts', 'movies']
     },
     {
@@ -23,19 +20,16 @@ export const questionSet = [
                 {musicGenres: ['Pop', 'Rock', 'Country', 'Hard Core', 'Dance', 'Alternative', 'Hip-Hop', 'R&B/Soul', 'Rap']},
                 {podcastGenres: ['Comedy', 'Daily News', 'History', 'Documentary', 'Technology', 'True Crime', 'Education', 'Sports', 'Relationships', 'Design', 'Music', 'Science']},
                 {movieGenres: ['Adventure', 'Animation', 'Documentary', 'Comedy', 'Family', 'Fantasy', 'Horror', 'Mystery', 'Sci-Fi']}],
-     answers: [],      
      activities:['music','podcasts','movies'],
     },
     {
       question: 'Would you like your movie to be shorter than 2 hours?', 
       choices: ['yes', 'no'], 
-      answers: [],
       activities: ['movies']
     },
     {
       question: 'How many people are playing?',
       choices: ['1', '2', '3', '4', '5', 'more than 5'],
-      answers: [],
       activities: ['board games', 'card games']
      }
   ]
