9f7ab008a58726cde3fa7d74127747982beec61f
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-10-30 18:32:46 -0600

    Create questionSet array

---
+++ src/Components/Form/questions.js
@@ -0,0 +1,29 @@
+export const questionSet = [
+    {
+      question: 'How old is too old?',
+      choices: [2010, 2000, 1990, 1980, "No such thing as too old"],
+      answers: [],
+      activities: ['music', 'podcasts', 'movies']
+    },
+    {
+      question: 'Which genre(s)?',
+      choices: [
+                {musicGenres: ['Pop', 'Rock', 'Country', 'Hard Core', 'Dance', 'Alternative', 'Hip-Hop', 'R&B/Soul', 'Rap']},
+                {podcastGenres: ['Comedy', 'Daily News', 'History', 'Documentary', 'Technology', 'True Crime', 'Education', 'Sports', 'Relationships', 'Design', 'Music', 'Science']},
+                {movieGenres: ['Adventure', 'Animation', 'Documentary', 'Comedy', 'Family', 'Fantasy', 'Horror', 'Mystery', 'Sci-Fi']}],
+     answers: [],      
+     activities:['music','podcasts','movies'],
+    },
+    {
+      question: 'Would you like your movie to be shorter than 2 hours?', 
+      choices: ['yes', 'no'], 
+      answers: [],
+      activities: ['movies']
+    },
+    {
+      question: 'How many people are playing?',
+      choices: ['1', '2', '3', '4', '5', 'more than 5'],
+      answers: [],
+      activities: ['board games', 'card games']
+     }
+  ]
