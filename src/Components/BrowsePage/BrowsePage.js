0bf0ed35755c82df4cbe57996c31f28d0a303433
Orlando Murcio <ato.murcio@gmail.com>
2020-11-02 14:41:58 -0700

    Display movie data

--- src/Components/BrowsePage/BrowsePage.js
+++ src/Components/BrowsePage/BrowsePage.js
@@ -2,56 +2,39 @@ import React from 'react'
 import './BrowsePage.scss'
 import { CgUserlane } from "react-icons/cg";
 import { RiHomeSmileLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 export const BrowsePage = (props) => {
+    console.log(props)
+    const injectActivityInformation = () => {
+        return props.movies.map(e => {
+            return (
+                <li className="hexa">
+                    <div className="inner-hexagon">
+                    <img className="hexa-image" src={e.image_poster} alt=""/>
+                    </div>
+                </li>
+          )
+        })
+    }
 
   return (
     <section className="movies-section">
         <div className="bar-menu">
           <CgUserlane className="logo"/>
-          <RiHomeSmileLine className="logo"/>
+          
+          <Link to='/'>
+            <RiHomeSmileLine className="logo"/>
+          </Link>
         </div>
-
+        
+        <div className="title-container">
+            <h1 className="title">Activitities</h1>
+        </div>
+        
         <body className="grid-container">
             <ul className="inner-grid">
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-                <li className="hexa">
-                  <div class="inner-hexagon"></div>
-                </li>
-
+                {injectActivityInformation()}
             </ul>
         </body>
 
