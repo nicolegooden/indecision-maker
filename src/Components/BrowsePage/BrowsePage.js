1fc59b789889688633262e575f39bbe0de5d5d4a
Orlando Murcio <ato.murcio@gmail.com>
2020-11-02 17:56:03 -0700

    Add conditional render logit to display images and titles

--- src/Components/BrowsePage/BrowsePage.js
+++ src/Components/BrowsePage/BrowsePage.js
@@ -5,13 +5,20 @@ import { RiHomeSmileLine } from "react-icons/ri";
 import { Link } from 'react-router-dom';
 
 export const BrowsePage = (props) => {
-    console.log(props)
+    console.log(props.data)
     const injectActivityInformation = () => {
-        return props.movies.map(e => {
+        return props.data.map((e, i) => {
             return (
-                <li className="hexa">
+                <li key ={i}className="hexa">
                     <div className="inner-hexagon">
-                    <img className="hexa-image" src={e.image_poster} alt=""/>
+                        <img className="hexa-image" 
+                        src={e.image_poster || e.image_100 || e.image} 
+                        alt={e.song_title || e.instructions || e.podcast_title || e.name}
+                        />
+                    {e.name && <h1 className={e.name.length  < 20 ? "title" : "large-title"}>{e.name }</h1>}
+                    {e.podcast_title && <h1 className={e.podcast_title.length  < 20 ? "title": "large-title"}>{e.podcast_title }</h1>}
+                    {e.song_title && <h1 className={e.song_title.length  < 20 ? "title" : "large-title"}>{e.song_title }</h1>}
+                    {e.title && <h1 className={e.title.length  < 20 ? "title" : "large-title"}>{e.title }</h1>}
                     </div>
                 </li>
           )
@@ -22,14 +29,14 @@ export const BrowsePage = (props) => {
     <section className="movies-section">
         <div className="bar-menu">
           <CgUserlane className="logo"/>
-          
+
           <Link to='/'>
             <RiHomeSmileLine className="logo"/>
           </Link>
         </div>
         
         <div className="title-container">
-            <h1 className="title">Activitities</h1>
+            <h1 className="title">{props.name}</h1>
         </div>
         
         <body className="grid-container">
