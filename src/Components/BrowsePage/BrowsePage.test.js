c06a32b9b02c767587d566eb8648a49793dc961a
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-04 18:27:14 -0500

    Add tests for movie and boardGame images

--- src/Components/BrowsePage/BrowsePage.test.js
+++ src/Components/BrowsePage/BrowsePage.test.js
@@ -4,10 +4,24 @@ import { render, screen } from "@testing-library/react";
 import {MemoryRouter} from 'react-router-dom'
 import "@testing-library/jest-dom";
 
+
+
 describe.only("BrowsePage", () => {
-    it("Should load with a list of activities", () => {
-        render(<MemoryRouter><BrowsePage name={"boardGames"} data={[{song_title: 'Test Song'}]}/></MemoryRouter>);
+    it("Should load with activities", () => {
+        render(<MemoryRouter><BrowsePage name={"music"} data={[{song_title: 'Test Song'}]}/></MemoryRouter>);
         expect(screen.getByText('Test Song')).toBeInTheDocument();
-    screen.debug()
+    });
+
+    it("Bord Games should load with game images", () => {
+        render(<MemoryRouter><BrowsePage name={"boardGames"} data={[{name: 'Test Game', image: 'boardTestURL'}]}/></MemoryRouter>);
+        expect(screen.getByText('Test Game')).toBeInTheDocument();
+        expect(screen.getByTestId('image-test')).toHaveProperty('src', 'http://localhost//boardTestURL');
+    });
+
+    it("Movies should load with poster images", () => {
+        render(<MemoryRouter><BrowsePage name={"movies"} data={[{title: 'Test Movie', image_poster: 'movieTestURL'}]}/></MemoryRouter>);
+        expect(screen.getByText('Test Movie')).toBeInTheDocument();
+        expect(screen.getByTestId('image-test')).toHaveProperty('src', 'http://localhost//movieTestURL')
+        screen.debug()
     });
 });
