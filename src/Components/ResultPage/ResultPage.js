5019b3941cb886ae80d70fe0474461eff0cef462
Orlando Murcio <ato.murcio@gmail.com>
2020-11-03 23:09:56 -0700

    Display correct data on galley section

--- src/Components/ResultPage/ResultPage.js
+++ src/Components/ResultPage/ResultPage.js
@@ -2,28 +2,49 @@ import React from 'react';
 import './ResultPage.scss';
 import { CgUserlane } from "react-icons/cg";
 import { RiHomeSmileLine } from "react-icons/ri";
-import { GoLightBulb } from "react-icons/go";
+import { FaLightbulb } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+
 export const ResultPage = (props) => {
   console.log(props)
   const noImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png"
   const displayActivityGallery = () => {
-    return props.data.map(activity => {
-    console.log()
+    return props.data.map((activity, i) => {
+
     return (
-      <div className="inner-all-rest">
+      <div key={i}className="inner-all-rest">
         <div className="border">
-          <GoLightBulb className="bulb-icon"/>
+          <FaLightbulb className="bulb-icon"/>
           <h3 className="title">{
                activity.song_title 
             || activity.title 
             || activity.podcast_title 
+            || activity.podcast_name
             || activity.name
             || "no information"}
           </h3>
-          <h3 className="title">more information</h3>
-          <h3 className="title">more information</h3>
-          <h3 className="title">more information</h3>
+          <h3 className="title">{
+               activity.podcast_name
+            || activity.artist
+            || activity.materials
+            || "no information available"
+            }
+          </h3>
+          <h3 className="title">{
+               activity.release_date
+            || activity.author
+            || activity.materials
+            || "no information available"
+            }
+          </h3>
+          <h3 className="title">{
+               activity.genre
+            || activity.average_time
+            || activity.number_of_players
+            || activity.type
+            || "no information available"
+            }
+          </h3>
         </div>
       </div>
     )
@@ -44,15 +65,27 @@ export const ResultPage = (props) => {
           <div className="slider">
 
             <h1 className="activity-title">{
-                   props.randomActivity.song_title 
-                || props.randomActivity.title 
-                || props.randomActivity.podcast_title 
-                || props.randomActivity.name
-                || "no information"}
+                props.randomActivity.song_title
+              || props.randomActivity.title 
+              || props.randomActivity.podcast_title 
+              || props.randomActivity.name
+              || "no information"
+            }
+            </h1>
+            <h2 className="activity-title">{
+                props.randomActivity.podcast_name
+              || props.randomActivity.artist
+              || props.randomActivity.materials
+              || props.randomActivity.content_rating
+              || "no information available"
+            }
+            </h2>
 
-              </h1>
+           <h2 className="activity-title">{
+                props.randomActivity.imdb_raiting
+            }</h2>
+
             <div className="slides">
-            
                 <div className="slide" id="slide-1">
                     <img className="image" 
                         src={props.randomActivity.image_poster 
@@ -62,7 +95,6 @@ export const ResultPage = (props) => {
                         alt=""/>
                 </div>
             </div>
-
           </div>
             
           <div className="activity-controls">
@@ -71,6 +103,7 @@ export const ResultPage = (props) => {
           </div>
 
           <div className="the-rest-container">
+
            {displayActivityGallery()}
           </div>
 
