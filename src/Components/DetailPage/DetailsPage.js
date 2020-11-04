1314173dfbe98ed0760bacb7017e837b319b4bdd
Orlando Murcio <ato.murcio@gmail.com>
2020-11-04 11:06:18 -0700

    Display detials about activities

--- src/Components/DetailPage/DetailsPage.js
+++ src/Components/DetailPage/DetailsPage.js
@@ -5,8 +5,8 @@ import { RiHomeSmileLine } from "react-icons/ri";
 import { Link } from 'react-router-dom';
 
 export const DetailsPage = (props) => {
-    
-
+  const noImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png"
+  console.log(props)
     return (
         <div className="details-container">
           <div className="bar-menu">
@@ -18,16 +18,52 @@ export const DetailsPage = (props) => {
           </div>
 
           <div className="info activity-title">
-            
-            <h1 className="">information goes here</h1>
+            <h1 className="title">Activity Title</h1>
+            <h1 className="">{
+               props.randomActivity.song_title 
+            || props.randomActivity.title 
+            || props.randomActivity.podcast_title 
+            || props.randomActivity.podcast_name
+            || props.randomActivity.name
+            || "no information"}</h1>
           </div>
 
           <div className="info activity-information">
             <div className="image-container">
-              <img src="https://images.unsplash.com/photo-1533618821901-9e69d5f5360e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>
+              <img 
+              src={props.randomActivity.image_poster 
+                  || props.randomActivity.image_100 
+                  || props.randomActivity.image 
+                  || noImage
+                  } alt=""/>
             </div>
-            <p className="content">Here is a lot of the content, just in case you were wondering</p>
+
+            <p className="content">{
+               props.randomActivity.podcast_name
+            || props.randomActivity.artist
+            || props.randomActivity.title 
+            || "no information available"
+            }</p>
+
+            
+            {props.randomActivity.episode_count && <p className="content">{props.randomActivity.episode_count } episodes</p>}
+            
+            {props.randomActivity.average_time && <h4 className="title">Average play time</h4>}
+            {props.randomActivity.average_time && <p className="content">{props.randomActivity.average_time}</p>}
+
+            {props.randomActivity.imdb_rating && <h4 className="title">Imdb Raiting</h4>}
+            {props.randomActivity.imdb_rating && <p className="content">{props.randomActivity.imdb_rating || 0}</p>}
+
+            {props.randomActivity.content_rating&& <h4 className="title">Content</h4>}
+            {props.randomActivity.content_rating && <p className="content">{props.randomActivity.content_rating|| 0}</p>}
+
+            {props.randomActivity.album_url && <h4 className="title">Listen here</h4>}
+            {props.randomActivity.album_url && <a className="content" href="props.randomActivity.album_url "> Click Here</a>}
+
+            {props.randomActivity.author && <h4 className="title">Author</h4>}
+            {props.randomActivity.author && <p >{props.randomActivity.author}</p>}
           </div>
+      
 
           <div className="info activity-more-info">
             <h1 className="">information goes here</h1>
