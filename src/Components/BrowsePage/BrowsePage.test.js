6ed209e391d181c8fbc5d76951715fe7015df315
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-04 19:33:56 -0500

    Add initial structure for games

--- src/Components/BrowsePage/BrowsePage.test.js
+++ src/Components/BrowsePage/BrowsePage.test.js
@@ -7,7 +7,7 @@ import {getAllMovies, getAllBoardGames, getAllCardGames, getAllMusic, getAllPodc
 jest.mock("../../apiCalls.js");
 
 describe("BrowsePage", () => {
-    it("Board Games should load with title and image", async () => {
+    it("Podcasts should load with podcast art and title ", async () => {
         let testResults;
         let podcastResults = getAllPodcasts.mockResolvedValue([{
           podcast_title: "The Joe Rogan Experience",
@@ -19,9 +19,9 @@ describe("BrowsePage", () => {
         expect(screen.getByAltText('The Joe Rogan Experience')).toBeInTheDocument();
     });
 
-    it.only("Music should load with Album cover and title", async () => {
+    it("Music should load with album cover and title", async () => {
         let testResults;
-        let musicResults = getAllMovies.mockResolvedValue([{
+        let musicResults = getAllMusic.mockResolvedValue([{
           song_title: "Poker Face",
           image_100: "musicURL"
         }]);
@@ -31,9 +31,40 @@ describe("BrowsePage", () => {
         expect(screen.getByAltText('Poker Face')).toBeInTheDocument();
     });
 
-    it("Movies should load with poster images", () => {
-        render(<MemoryRouter><BrowsePage name={"movies"} data={[{title: 'Test Movie', image_poster: 'movieTestURL'}]}/></MemoryRouter>);
+    it("Movies should load with movie poster and title", async () => {
+        let testResults;
+        let movieResults = getAllMovies.mockResolvedValue([{
+          song_title: "Harry Potter",
+          image_100: "Harry Potter"
+        }]);
+        await waitFor(async () => testResults = await movieResults())
+        render(<MemoryRouter><BrowsePage name={"movies"} data={testResults}/></MemoryRouter>);
         expect(screen.getByText('Test Movie')).toBeInTheDocument();
-        expect(screen.getByTestId('image-test')).toHaveProperty('src', 'http://localhost/movieTestURL')
+        expect(screen.getByText('Harry Potter')).toBeInTheDocument();
     });
+
+    it("Board Games should load with game image and title", async () => {
+        let testResults;
+        let boardResults = getAllBoardGames.mockResolvedValue([{
+          song_title: "",
+          image_100: "Harry Potter"
+        }]);
+        await waitFor(async () => testResults = await boardResults())
+        render(<MemoryRouter><BrowsePage name={"movies"} data={testResults}/></MemoryRouter>);
+        expect(screen.getByText('Test Movie')).toBeInTheDocument();
+        expect(screen.getByText('Harry Potter')).toBeInTheDocument();
+    });
+
+    it("Card Games should load with a name and min / max players", async () => {
+        let testResults;
+        let boardResults = getAllBoardGames.mockResolvedValue([{
+          song_title: "Harry Potter",
+          image_100: "Harry Potter"
+        }]);
+        await waitFor(async () => testResults = await boardResults())
+        render(<MemoryRouter><BrowsePage name={"movies"} data={testResults}/></MemoryRouter>);
+        expect(screen.getByText('Test Movie')).toBeInTheDocument();
+        expect(screen.getByText('Harry Potter')).toBeInTheDocument();
+    });
+
 });
