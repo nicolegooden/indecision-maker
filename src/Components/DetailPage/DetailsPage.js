79c004fe22d65be0c53784b62489b88bac85ba0e
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-20 12:57:45 -0700

    Fix typos and phrasing

--- src/Components/DetailPage/DetailsPage.js
+++ src/Components/DetailPage/DetailsPage.js
@@ -54,11 +54,11 @@ export const DetailsPage = (props) => {
                props.randomActivity.podcast_name
             || props.randomActivity.artist
             || props.randomActivity.title 
-            || "more information comming up soon"
+            || "More information coming soon"
             }</p>
             {props.randomActivity.episode_count && <p className="content">{props.randomActivity.episode_count } episodes</p>}
-            {props.randomActivity.average_time && <h4 className="title">Average play time</h4>}
-            {props.randomActivity.average_time && <p className="content">{props.randomActivity.average_time}</p>}
+            {props.randomActivity.average_time && <h4 className="title">Average play time:</h4>}
+            {props.randomActivity.average_time && <p className="content">{props.randomActivity.average_time} mins</p>}
             {props.randomActivity.materials && <h4 className="title">Materials</h4>}
             {props.randomActivity.materials && <p className="content">{props.randomActivity.materials}</p>}
             {props.randomActivity.imdb_rating && <h4 className="title">Imdb Raiting</h4>}
@@ -71,16 +71,16 @@ export const DetailsPage = (props) => {
             {props.randomActivity.author && <p >{props.randomActivity.author}</p>}
           </div>
           <div className="info activity-more-info">
-            <h1 className="">A little bit about me</h1>
+            <h1 className="">Activity Details</h1>
             {props.randomActivity.brief_description && <p className="content">{props.randomActivity.brief_description || props.randomActivity.brief_description}</p>}
             {props.randomActivity.description && <p className="content">{props.randomActivity.description}</p>}
-            {props.randomActivity.artist && <h3 className="title">Artis name</h3>}
+            {props.randomActivity.artist && <h3 className="title">Artist</h3>}
             {props.randomActivity.artist  && <p className="content">{props.randomActivity.artist}</p>}
             {props.randomActivity.podcast_name && <h3 className="title">Podcast name</h3>}
             {props.randomActivity.podcast_name  && <p className="content">{props.randomActivity.podcast_name}</p>}
           </div>
           <div className="info activity-more-more-info">
-            {props.randomActivity.instructions &&<h1 className="">intructions</h1>}
+            {props.randomActivity.instructions &&<h1 className="">instructions</h1>}
             {props.randomActivity.instructions && <p className="content">{props.randomActivity.instructions }</p>}
             {props.randomActivity.type && <h3 className="title">Type</h3>}
             {props.randomActivity.type && <p className="content">{props.randomActivity.type}</p>}
